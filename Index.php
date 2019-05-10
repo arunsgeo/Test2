cd0999fba9f472a3b5b0e4703b0b7e899d877150
Arun George <arun.g@innoppl.com>
2019-05-10 19:39:41 +0530

    Added a new line of code in index.php at 7:33 PM on May 10, 2019

--- Index.php
+++ Index.php
@@ -1,4 +1,6 @@
 <?php
 echo "Hello World";
 echo "Changes from the local machine";
+echo "Added this code from Windows machine at 7:33 PM on may 10, 2019";
+echo "Added this code from Windows machine at 7:39 PM on may 10, 2019";
 ?>
